94081486b3d8d9b775090a9636e0c50392556967
Steven Bressey <sbressey@kaliop.com>
2015-08-12 09:24:03 +0200

    refactored service injection in Twig extension

--- WrapperBundle/Twig/eZObjectWrapperExtension.php
+++ WrapperBundle/Twig/eZObjectWrapperExtension.php
@@ -11,22 +11,15 @@ use eZSys;
 
 class eZObjectWrapperExtension extends \Twig_Extension
 {
-
     /**
-     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     * @var ViewController
      */
-    private $container;
+   private $viewController;
 
-    /**
-     * @var \eZ\Publish\API\Repository\Repository
-     */
-    private $repository;
+    public function __construct(\eZ\Publish\Core\MVC\Symfony\Controller\Content\ViewController $viewController) {
 
-    public function __construct($container = null) {
-        if ($container) {
-            $this->container  = $container;
-            $this->repository = $this->container->get('ezpublish.api.repository');
-        }
+      $this->viewController = $viewController;
+
     }
 
     public function getFunctions() {
@@ -37,11 +30,11 @@ class eZObjectWrapperExtension extends \Twig_Extension
      * Render a location according to the viewType and the ContentType set in override.yml
      * @param $locationID integer
      * @param $viewType string
-     * @param null $params
+     * @param array $params
      * @return string
      */
     public function renderLocation($locationID, $viewType, $params = array()) {
-        $rendering = $this->container->get('ez_content')->viewLocation($locationID, $viewType, false, $params);
+        $rendering = $this->viewController->viewLocation($locationID, $viewType, false, $params);
 
         return htmlspecialchars_decode($rendering->getContent());
     }
