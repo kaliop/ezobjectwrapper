55f772dd278dd228f2a73e4f95afc84767d909c0
gggeek <ggiunta@kaliop.com>
2015-10-25 23:37:18 +0000

    Add typehinting to support IDEs

--- WrapperBundle/Core/eZObjectWrapperService.php
+++ WrapperBundle/Core/eZObjectWrapperService.php
@@ -4,12 +4,14 @@ namespace Kaliop\eZObjectWrapperBundle\Core;
 
 use eZ\Publish\API\Repository\Values\Content\Content;
 use eZ\Publish\API\Repository\Values\Content\Location;
-use \eZ\Publish\API\Repository\Repository;
 
 /**
  * A 'factory-like' service, used to create new instances of itself.
  * It can be subclassed to inject more dependencies into it, and registered in the service_map configuration.
- * NB: since services are singletons, the initFromContent and initFromLocation locations create new object instances each time.
+ *
+ * NB: since services are singletons, the initFromContent and initFromLocation locations create new object instances
+ * each time. If you subclass this class, remember to reimplement those to be sure to properly initialize the created
+ * instances with all the desired state.
  */
 class eZObjectWrapperService extends eZObjectWrapper
 {
@@ -17,12 +19,16 @@ class eZObjectWrapperService extends eZObjectWrapper
      * @param $content
      * @param $location
      */
-    protected function init($content, $location)
+    protected function init(Content $content=null, Location $location=null)
     {
         $this->content = $content;
         $this->location = $location;
     }
 
+    /**
+     * @param Content $content
+     * @return static
+     */
     public function initFromContent(Content $content)
     {
         $wrapper = new static($this->repository);
@@ -30,6 +36,10 @@ class eZObjectWrapperService extends eZObjectWrapper
         return $wrapper;
     }
 
+    /**
+     * @param Location $location
+     * @return static
+     */
     public function initFromLocation(Location $location)
     {
         $wrapper = new static($this->repository);
