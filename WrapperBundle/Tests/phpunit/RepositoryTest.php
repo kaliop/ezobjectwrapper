78b09ec87d16243c18000fdc9b0c73f324088452
gggeek <ggiunta@kaliop.com>
2015-11-08 18:09:39 +0000

    Add more tests; on Travis, test on eZPlatform Beta7

--- WrapperBundle/Tests/phpunit/RepositoryTest.php
+++ WrapperBundle/Tests/phpunit/RepositoryTest.php
@@ -4,6 +4,15 @@ include_once(__DIR__.'/BaseTest.php');
 
 class RepositoryTest extends BaseTest
 {
+    public function testLoadByLocationId()
+    {
+        $entityManager = $this->container->get('ezobject_wrapper.entity_manager');
+        $repo = $entityManager->getRepository('any');
+
+        $e2 = $repo->loadEntityFromLocationId($this->rootEntity->location()->id);
+        $this->assertEquals($this->rootEntity->content()->id, $e2->content()->id);
+    }
+
     public function testLoadByRemoteId()
     {
         $entityManager = $this->container->get('ezobject_wrapper.entity_manager');
