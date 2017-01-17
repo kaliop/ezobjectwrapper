0474220bd48d107735b3398a8aef29a84af9c276
gggeek <ggiunta@kaliop.com>
2017-01-17 11:07:45 +0000

    travis fixes

--- WrapperBundle/Tests/setup/setup-content.sh
+++ WrapperBundle/Tests/setup/setup-content.sh
@@ -3,4 +3,4 @@
 EZ_VERSION=$1
 EZ_APP_DIR=$2
 
-php vendor/ezsystems/${EZ_VERSION}/${EZ_APP_DIR}/console --env=behat kaliop:migration:migrate -n --path=WrapperBundle/Tests/data/001_Content.yml
+php vendor/ezsystems/${EZ_VERSION}/${EZ_APP_DIR}/console --env=behat kaliop:migration:migrate -n -u --path=WrapperBundle/Tests/data/001_Content.yml
